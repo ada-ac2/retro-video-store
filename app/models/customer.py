9f36de080f85a7c21ea85b9302720a9cd850e8a8
kelly <kelly.tate.dev@gmail.com>
2023-01-03 13:53:36 -0800

    Updated Customer Model

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,7 +2,7 @@ from app import db
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    postal_code = db.Column(db.String(5))
-    phone_number = db.Column(db.String(20))
+    postal_code = db.Column(db.String(5), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
     register_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
-    videos_checked_out_count = db.Column(db.Integer)
+    videos_checked_out_count = db.Column(db.Integer, nullable=False)
