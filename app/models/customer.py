082b74ea3bbf076da4361bfec287dd58289a33b4
kelly <kelly.tate.dev@gmail.com>
2023-01-03 13:51:48 -0800

    Added Customer Model

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,3 +2,7 @@ from app import db
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    postal_code = db.Column(db.String(5))
+    phone_number = db.Column(db.String(20))
+    register_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+    videos_checked_out_count = db.Column(db.Integer)
