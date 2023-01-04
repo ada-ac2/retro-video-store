f29ab5092b0a2e1b3a0c2e67836b4ac2f14d543a
Ya-Juan Ruan <yajuan@decltype.org>
2023-01-03 20:55:23 -0800

    Add customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,11 @@
 from app import db
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.functions import now
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    phone_number = db.Column(db.String)
+    register_at = db.Column(db.DateTime, server_default=now())
+    videos_checked_out_count = db.Column(db.Integer)
+
