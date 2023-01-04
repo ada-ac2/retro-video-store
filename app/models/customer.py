9f10b69fb4396d53e8a899d38f0bb7d22fadcffa
larissa <larza.ault@gmail.com>
2023-01-04 13:05:13 -0800

    Added customer class attributes and datatypes

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,3 +2,20 @@ from app import db
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    postal_code = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.String, nullable=False)
+    registered_at = db.Column(db.DateTime, nullable=False)
+    # videos_checked_out = db.Column(db.Integer)
+
+    def to_dict(self):
+        """
+        Returns dictionary of customer data.
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "postal code": self.postal_code,
+            "phone number": self.phone_number,
+            "registered at": self.registered_at,
+        }
