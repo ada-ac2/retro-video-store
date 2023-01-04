5caec9b37298e94328b79809d5e53b88ec0519d7
Ya-Juan Ruan <yajuan@decltype.org>
2023-01-03 23:18:34 -0800

    Update phone attribute name to aligh with test

--- app/models/customer.py
+++ app/models/customer.py
@@ -6,7 +6,7 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     postal_code = db.Column(db.String)
-    phone_number = db.Column(db.String)
+    phone = db.Column(db.String)
     register_at = db.Column(db.DateTime, server_default=now())
     videos_checked_out_count = db.Column(db.Integer)
 
