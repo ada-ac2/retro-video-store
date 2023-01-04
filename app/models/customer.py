8c993d0b3505687208149a5c5640c6788d81992c
maple-megan333 <maple.megan333@gmail.com>
2023-01-03 17:48:53 -0800

    First models complete. Co-authored-by: HienXuanPham <HienXuanPham@users.noreply.github.com>

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,13 @@
 from app import db
+import datetime
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    postal_code = db.Column(db.String)
+    phone_number = db.Column(db.String)
+    register_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    videos_checked_out_count = db.Column(db.Integer)
+    
+
+
--- app/models/video.py
+++ app/models/video.py
@@ -1,4 +1,7 @@
 from app import db
 
 class Video(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title=db.Column(db.String)
+    release_date = db.Column(db.Date)
+    total_inventory = db.Column(db.Integer)
