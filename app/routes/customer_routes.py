e59f37d8f8db2b2349ea0e6763171ffb4bba12c7
kelly <kelly.tate.dev@gmail.com>
2023-01-04 08:48:53 -0800

    Wave One - Added create_customer and get_customers

--- app/routes/customer_routes.py
+++ app/routes/customer_routes.py
@@ -9,7 +9,32 @@ def create_customer():
     customer_data = request.get_json()
 
     new_customer = Customer(
+        name = customer_data["name"],
         postal_code = customer_data["postal_code"],
-        phone_number = customer_data["phone_number"],
-        register_at = datetime.datetime.utcnow()
+        phone = customer_data["phone_number"],
+        videos_checked_out_count = 0
     )
+
+    db.session.add(new_customer)
+    db.session.commit()
+
+    return make_response(f"Customer {new_customer.name} created", 201)
+
+@customers_bp.route("", methods=["GET"])
+def get_customers():
+    customer_query = Customer.query
+
+    customers = customer_query.all()
+    customer_response = []
+
+    for customer in customers:
+        customer_response.append({
+            "id": customer.id,
+            "name": customer.name,
+            "registered_at": customer.registered_at,
+            "postal_code": customer.postal_code,
+            "phone": customer.phone,
+            "videos_checked_out_count": customer.videos_checked_out_count
+        })
+    
+    return jsonify(customer_response)
