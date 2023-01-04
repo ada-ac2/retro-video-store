89b4ecff48457d218084bcdd9dd7d2557d0f14fa
kelly <kelly.tate.dev@gmail.com>
2023-01-04 11:12:11 -0800

    Added model_helpers

--- app/routes/customer_routes.py
+++ app/routes/customer_routes.py
@@ -1,5 +1,6 @@
 from app import db
 from app.models.customer import Customer
+import app.models.model_helpers
 from flask import Blueprint, jsonify, abort, make_response, request
 
 customers_bp = Blueprint("customers_bp", __name__, url_prefix="/customers")
