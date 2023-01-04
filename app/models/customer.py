d7a424ed5b203949ba89b051ed48d7a9adfda4db
kelly <kelly.tate.dev@gmail.com>
2023-01-04 08:15:33 -0800

    Updates Customer and Video to autoincrement id

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,8 +2,8 @@ from app import db
 import datetime
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     postal_code = db.Column(db.String(5), nullable=False)
     phone_number = db.Column(db.String(20), nullable=False)
     register_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
-    videos_checked_out_count = db.Column(db.Integer, nullable=False)
+    videos_checked_out_count = db.Column(db.Integer)
