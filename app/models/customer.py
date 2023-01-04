cac07470f5c46a4f5e05ea29ac112457f5465586
maple-megan333 <maple.megan333@gmail.com>
2023-01-03 18:09:50 -0800

    Made dictionary methods for models!

--- app/models/customer.py
+++ app/models/customer.py
@@ -9,5 +9,23 @@ class Customer(db.Model):
     register_at = db.Column(db.DateTime, default=datetime.datetime.now())
     videos_checked_out_count = db.Column(db.Integer)
     
+    def to_dict(self):
+        customer_dict = {}
+        customer_dict["id"] = self.id
+        customer_dict["name"] = self.name
+        customer_dict["postal_code"]=self.postal_code
+        customer_dict["phone_number"] = self.phone_number
+        
+        return customer_dict
 
 
+    @classmethod
+    def from_dict(cls,customer_data):
+        new_customer = Customer(
+            name=customer_data["name"],
+            postal_code=customer_data["postal_code"],
+            phone_number=customer_data["phone_number"],
+            register_at=customer_data["register_at"],
+            videos_checked_out_count=customer_data["videos_checked_out_count"]     
+        )
+        return new_customer
--- app/models/video.py
+++ app/models/video.py
@@ -5,3 +5,19 @@ class Video(db.Model):
     title=db.Column(db.String)
     release_date = db.Column(db.Date)
     total_inventory = db.Column(db.Integer)
+
+    def to_dict(self):
+        video_dict = {}
+        video_dict["id"] = self.id
+        video_dict["title"] = self.title
+        video_dict["release_date"] = self.release_date
+        video_dict["total_inventory"] = self.total_inventory
+
+        return video_dict
+
+    @classmethod
+    def from_dict(cls,video_data):
+        new_video = Video(title=video_data["title"], release_date=video_data["release_date"],
+        total_inventory=video_data["total_inventory"])
+        
+        return new_video
