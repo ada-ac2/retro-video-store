6fb7766395694fa9c1f76b0550f45dadc1a2215f
yves <dwu415@gmail.com>
2023-01-04 23:46:53 -0800

    add validation for delete and update request

--- app/models/video.py
+++ app/models/video.py
@@ -5,7 +5,7 @@ class Video(db.Model):
     title = db.Column(db.String, nullable = False)
     release_date = db.Column(db.Date, nullable = False)
     total_inventory = db.Column(db.Integer, default = 0, nullable = False)
-    available_inventory = db.Column(db.Integer, default = 0, nullable = False)
+    #available_inventory = db.Column(db.Integer, default = 0, nullable = False)
 
     def to_dict(self):
         video_as_dict = dict()
@@ -13,15 +13,15 @@ class Video(db.Model):
         video_as_dict["title"] = self.title
         video_as_dict["release_date"] = self.release_date
         video_as_dict["total_inventory"] = self.total_inventory
-        video_as_dict["available_inventory"] = self.available_inventory
+        #video_as_dict["available_inventory"] = self.available_inventory
         return video_as_dict
     
     @classmethod
     def from_dict(cls, video_data):
         new_video = Video(
             title=video_data["title"],
-            release_Date=video_data["release_date"],
+            release_date=video_data["release_date"],
             total_inventory = video_data["total_inventory"],
-            available_inventory=video_data["available_inventory"]
+            #available_inventory=video_data["available_inventory"]
         )
         return new_video
--- app/routes/video_routes.py
+++ app/routes/video_routes.py
@@ -14,8 +14,21 @@ def validate_video(cls, model_id):
     
     model = cls.query.get(model_id)
     if not model:
-        abort(make_response({"message": f"{cls.__name__} {model_id} is not found"}, 404))
+        abort(make_response({"message": f"{cls.__name__} {model_id} was not found"}, 404))
     return model
+
+
+def validate_record(video):
+    invalid_dict = dict()
+    if "title" not in video or not isinstance(video["title"], str) or video["title"] is None:
+        invalid_dict["details"] = "Request body must include title."
+    if "release_date" not in video or not isinstance(video["release_date"], str) \
+        or video["release_date"] is None:
+        invalid_dict["details"] = "Request body must include release_date."
+    if "total_inventory" not in video or not isinstance(video["total_inventory"], int) \
+        or video["total_inventory"] < 0:
+        invalid_dict["details"] = "Request body must include total_inventory."
+    return invalid_dict
 
 # GET /videos
 # The API should return an empty array and a status 200 if there are no videos.
@@ -42,12 +55,18 @@ def get_video_by_id(id):
 # The API should return back detailed errors and 
 # a status 400: Bad Request if the video does not have any of 
 # the required fields to be valid.
-@video_bp.route("", method=["POST"])
+@video_bp.route("", methods=["POST"])
 def create_video():
     request_body = request.get_json()
+    check_invalid_record = validate_record(request_body)
+    if check_invalid_record:
+        return abort(make_response(jsonify(check_invalid_record), 400))
+
     new_video = Video.from_dict(request_body)
+    
     db.session.add(new_video)
     db.session.commit()
+    db.session.refresh(new_video)
     return make_response(jsonify(f"Video {new_video.title} successfully created"), 201)
 
 
@@ -57,23 +76,26 @@ def create_video():
 # The API should return back a 400 Bad Request response for 
 # missing or invalid fields in the request body.
 # For example, if total_inventory is missing or is not a number
-@video_bp.route("/<id>", method=["PUT"])
+@video_bp.route("/<id>", methods=["PUT"])
 def update_video(id):
     video = validate_video(Video, id)
     request_body = request.get_json()
+    check_invalid_record = validate_record(request_body)
+    if check_invalid_record:
+        return abort(make_response(jsonify(check_invalid_record), 400))
     video.title = request_body["title"]
     video.release_date = request_body["release_date"]
     video.total_inventory = request_body["total_inventory"]
-    video.available_inventory = request_body["available_inventory"]
+    #video.available_inventory = request_body["available_inventory"]
     return make_response(jsonify(f"Video #{id} successfully updated"))
 
 
 # DELETE /videos/<id>
 # The API should return back detailed errors 
 # and a status 404: Not Found if this video does not exist.
-@video_bp.route("/<id>", method=["DELETE"])
+@video_bp.route("/<id>", methods=["DELETE"])
 def delete_video(id):
-    video = validate_video(id)
+    video = validate_video(Video, id)
     db.session.delete(video)
     db.session.commit()
     return make_response(jsonify(f"Video #{id} successfully deleted"))
