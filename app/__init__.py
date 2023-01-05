a64e5ef6eb1da97710dd9bfce8172b2ce3f63bd5
Cindy Truong <cindythitruong@gmail.com>
2023-01-04 21:31:53 -0800

    added blueprints for customer, video, and rental

--- app/__init__.py
+++ app/__init__.py
@@ -32,5 +32,13 @@ def create_app(test_config=None):
     migrate.init_app(app, db)
 
     #Register Blueprints Here
+    from .routes import customer_bp
+    app.register_blueprint(customer_bp)
+
+    from .routes import video_bp
+    app.register_blueprint(video_bp)
+
+    from .routes import authors_bp
+    app.register_blueprint(authors_bp)
 
     return app
