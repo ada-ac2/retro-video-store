6a0c9b0b96d826b2fe2fc7f736b4d5048c7e97b4
Jennifer Dai <jendai173@gmail.com>
2023-01-04 21:02:41 -0800

    wave 1 draft, working on debugging

--- app/models/video.py
+++ app/models/video.py
@@ -1,20 +1,24 @@
 from app import db
 
 class Video(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True,autoincrement = True)
     title = db.Column(db.String)
-    release_date = db.Column(db.String)
+    release_date = db.Column(db.DateTime)
     total_inventory = db.Column(db.Integer)
+
+    def to_dict(self):
+        video_as_dict = {}
+        video_as_dict["id"] = self.id
+        video_as_dict["title"] = self.title
+        video_as_dict["release_date"] = self.release_date
+        video_as_dict["total_inventory"] = self.total_inventory     
+
+        return video_as_dict
+
 
     @classmethod
     def from_dict(cls, video_data):
         new_video = Video(title = video_data["title"],
                         release_date=video_data["release_date"],
                         total_inventory = video_data['total_inventory'])
-        return video_data
-#     {
-#   "id": 1,
-#   "title": "Blacksmith Of The Banished",
-#   "release_date": "1979-01-18",
-#   "total_inventory": 10
-# }
+        return new_video
--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,8 @@ from app import db
 from app.models.video import Video 
 from app.models.customer import Customer
 
-customer_bp = Blueprint("customer_bp", __name__, url_prefix="/customers")
-video_bp = Blueprint("video_bp", __name__, url_prefix="/video")
+customers_bp = Blueprint("customers_bp", __name__, url_prefix="/customers")
+videos_bp = Blueprint("videos_bp", __name__, url_prefix="/videos")
 
 #--------------------------Helper Functions----------------------------------------------
 def validate_model(cls, model_id):
@@ -25,9 +25,16 @@ def validate_request_body(request_body):
         not in request_body or "postal_code" not in request_body:
         abort(make_response("Invalid Request", 400))
 
-#--------------------------- Route Functions -----------------------------------------
+#validation for Video route
+def validate_video_request_body(request_body): 
 
-@customer_bp.route("", method = ["POST"])
+    if "title" not in request_body or "release_date" not in request_body or "total_inventory" \
+        not in request_body:
+        abort(make_response("Invalid Request", 400))
+
+#--------------------------- Customer Route Functions -----------------------------------------
+
+@customers_bp.route("/customers", method = ["POST"])
 def create_customer():
     request_body = request.get_json()
     validate_request_body(request_body)
@@ -39,26 +46,38 @@ def create_customer():
 
     return make_response(jsonify(f"Customer: {new_customer.name} created successfully.", 201))
 
-@customer_bp.route("", method = ["GET"])
+@customers_bp.route("", method = ["GET"])
 def read_all_customers():
     #get a query object for later use
     customer_query = Customer.query
 
     customers = customer_query.all()
         
-    customer_response = [] 
-    for customer in customers: 
-        customer_response.append(customer.to_dict())    #use to_dict function to make code more readable
+    customers_response = [] 
 
-    return make_response(jsonify(customer_response), 200)
+    for customer in customers:
+        customers_response.append({
+            "id" : customer.id,
+            "name": customer.name,
+            "postal_code": customer.postal_code,
+            "phone": customer.phone,
+            "register_at": customer.register_at, 
+            #"videos_checked_out_count":customer.videos_checked_out_count
+        })
+    #return jsonify(customers_response)
+    
+    # for customer in customers: 
+    #     customer_response.append(customer.to_dict())    #use to_dict function to make code more readable
 
-@customer_bp.route("/<customer_id>", method = {"GET"})
+    return make_response(jsonify(customers_response), 200)
+
+@customers_bp.route("/<customer_id>", method = {"GET"})
 def read_one_customer_by_id(customer_id):
     customer = validate_model(Customer, customer_id)
 
     return (customer.to_dict(),200)
 
-@customer_bp.route("/<customer_id>", method = {"PUT"})
+@customers_bp.route("/<customer_id>", method = {"PUT"})
 def update_customer_by_id(customer_id):
     customer = validate_model(Customer, customer_id)
 
@@ -74,7 +93,7 @@ def update_customer_by_id(customer_id):
 
     return make_response(jsonify(f"Customer: {customer_id} has been updated successfully."), 200) 
 
-@customer_bp.route("/<customer_id>", methods = ["DELETE"])
+@customers_bp.route("/<customer_id>", methods = ["DELETE"])
 def delete_customer_by_id(customer_id): 
     customer = validate_model(Customer, customer_id)  
 
@@ -82,3 +101,65 @@ def delete_customer_by_id(customer_id):
     db.session.commit()
 
     return make_response(jsonify(f"Customer: {customer_id} has been deleted successfully."), 200) 
+
+######################
+######################
+#--------Video--------
+######################
+#--------------------------- Video Route Functions -----------------------------------------
+
+@videos_bp.route("", method = ["POST"])
+def create_one_video():
+    request_body = request.get_json()
+    validate_video_request_body(request_body)
+
+    new_video = Video.from_dict(request_body)
+
+    db.session.add(new_video)
+    db.session.commit()
+
+    return make_response(jsonify(f"Video: {new_video.title} created successfully.", 201))
+
+@videos_bp.route("", method = ["GET"])
+def read_all_videos():
+    #get a query object for later use
+    video_query = Video.query
+
+    videos = video_query.all()
+        
+    video_response = [] 
+    for video in videos: 
+        video_response.append(video.to_dict())    #use to_dict function to make code more readable
+    print(video_response)
+    return make_response(jsonify(video_response), 200)
+
+@videos_bp.route("/<video_id>", method = {"GET"})
+def read_one_video_by_id(video_id):
+    video = validate_model(Video, video_id)
+
+    return (video.to_dict(),200)
+
+@videos_bp.route("/<video_id>", method = {"PUT"})
+def update_video_by_id(video_id):
+    video = validate_model(Video, video_id)
+
+    request_body = request.get_json() 
+    validate_video_request_body(request_body)
+
+    video.title = request_body["title"]
+    video.release_date = request_body["release_date"]
+    video.total_inventory = request_body["total_inventory"]
+
+    db.session.commit() 
+
+    return make_response(jsonify(f"Video: {video_id} has been updated successfully."), 200) 
+
+@videos_bp.route("/<video_id>", methods = ["DELETE"])
+def delete_video_by_id(video_id): 
+    video = validate_model(Video, video_id)  
+
+    db.session.delete(video)
+    db.session.commit()
+
+    return make_response(jsonify(f"Video: {video_id} has been deleted successfully."), 200) 
+
