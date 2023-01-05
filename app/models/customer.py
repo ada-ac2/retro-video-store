43e954d1c718a8b28361a74456f6b6b1dba643c9
kelly <kelly.tate.dev@gmail.com>
2023-01-04 16:35:42 -0800

    Updates to customer model.

--- app/models/customer.py
+++ app/models/customer.py
@@ -3,8 +3,8 @@ import datetime
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String, nullable=False)
     registered_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
-    postal_code = db.Column(db.String(5), nullable=False)
-    phone = db.Column(db.String(20), nullable=False)
+    postal_code = db.Column(db.String, nullable=False)
+    phone = db.Column(db.String, nullable=False)
     videos_checked_out_count = db.Column(db.Integer)
