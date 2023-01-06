a62036af83e57dd5619893d6e5c2ba6e163aa53d
kelly <kelly.tate.dev@gmail.com>
2023-01-06 08:25:27 -0800

    Removes extra models

--- app/models/customer_rental.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-class CustomerRental(db.Model):
-    __tablename__ = "customer_rental"
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), primary_key=True,nullable=False)
-    rental_id = db.Column(db.Integer, db.ForeignKey('rental.id'), primary_key=True,nullable=False)
--- app/models/video_rental.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-class VideoRental(db.Model):
-    __tablename__ = "video_rental"
-    video_id = db.Column(db.Integer, db.ForeignKey('video.id'), primary_key=True,nullable=False)
-    rental_id = db.Column(db.Integer, db.ForeignKey('rental.id'), primary_key=True,nullable=False)
