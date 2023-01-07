29f62dc2edc9fcbfd255a54e413953be60f3e019
Soumya Sah <85212732+smysh@users.noreply.github.com>
2023-01-06 23:47:24 -0800

    added route to checkout video using POST /rentals/checkout

--- app/routes/rentals_routes.py
+++ app/routes/rentals_routes.py
@@ -1,6 +1,28 @@
 from app import db
 from flask import Blueprint, jsonify, make_response, abort, request
 from ..models.rental import Rental
+from ..models.customer import Customer
+from ..models.video import Video
+from ..routes.customer_routes import validate_model
+from ..routes.customer_routes import validate_request
 
 # ~~~~~~ initialize rentals blueprint ~~~~~~
 rentals_bp = Blueprint("rentals", __name__, url_prefix="/rentals")
+
+@rentals_bp.route("/check-out", methods=["POST"])
+def checkout_video():
+    reqs = {"customer_id", "video_id"}
+    request_body = validate_request(request, reqs)
+    video_id = request_body["video_id"]
+    video = validate_model(Video, video_id)
+    customer_id = request_body["customer_id"]
+    customer = validate_model(Customer, customer_id)
+    if video and customer:
+        new_rental = Rental(video_id=request_body["video_id"],customer_id=request_body["customer_id"])
+    
+        db.session.add(new_rental)
+        db.session.commit()
+
+        return make_response(jsonify(new_rental.to_dict()), 200)
+    else:
+        return make_response(jsonify({"message": f"Video {video_id} or Customer {customer_id} are invalid"}), 404)
