f04affb03a137ab9567550571ac1416603f5ecd1
larissa <larza.ault@gmail.com>
2023-01-06 19:33:48 -0800

    imported classes and blueprint for rentals endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -36,5 +36,7 @@ def create_app(test_config=None):
     app.register_blueprint(customers_bp)
     from app.routes.video_routes import video_bp
     app.register_blueprint(video_bp)
+    from app.routes.rentals_routes import rentals_bp
+    app.register_blueprint(rentals_bp)
 
     return app
