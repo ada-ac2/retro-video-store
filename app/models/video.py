f3c0a444ce39a6f62833fcd16cdeb87fb6984485
annadu <duyuan0921@gmail.com>
2023-01-08 09:49:35 -0800

    create video to dict

--- app/models/video.py
+++ app/models/video.py
@@ -5,3 +5,12 @@ class Video(db.Model):
     title = db.Column(db.String,nullable = False)
     release_date = db.Column(db.String)
     total_inventory = db.Column(db.Integer)
+
+
+    def to_dict(self):
+        video_dict = {}
+        video_dict["title"] = self.id
+        video_dict["release_date"] = self.release_date
+        video_dict["total_inventory"] = self.total_inventory
+        
+        return video_dict
