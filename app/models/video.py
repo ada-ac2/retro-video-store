9f9f2d4fb1705c1847e81c8974283a08bb4f2958
Soumya Sah <85212732+smysh@users.noreply.github.com>
2023-01-07 20:08:51 -0800

    Wave 1 tests are passing with these fixes.

--- app/models/video.py
+++ app/models/video.py
@@ -1,11 +1,14 @@
 from app import db
 
+def mydefault(context):
+        return context.get_current_parameters()['total_inventory']
+
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String())
     release_date = db.Column(db.Date())
     total_inventory = db.Column(db.Integer())
-    available_inventory = db.Column(db.Integer())
+    available_inventory = db.Column(db.Integer(),default=mydefault)
     rentals = db.relationship("Rental", back_populates="video")
 
     def to_dict(self):
@@ -14,7 +17,7 @@ class Video(db.Model):
         video_as_dict["title"] = self.title
         video_as_dict["release_date"] = self.release_date
         video_as_dict["total_inventory"] = self.total_inventory
-        
+        video_as_dict["available_inventory"] = self.available_inventory
         return video_as_dict
 
     def calculate_available_inventory(self):
@@ -28,6 +31,8 @@ class Video(db.Model):
         new_video = Video(
             title=video_data["title"],
             release_date=video_data["release_date"],
-            total_inventory=video_data["total_inventory"],
+            total_inventory=video_data["total_inventory"]
                         )
         return new_video
+
+
