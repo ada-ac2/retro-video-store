a7f93f7a8cfc89c246d9f341da1aa5a9cd7db645
annadu <duyuan0921@gmail.com>
2023-01-08 09:30:04 -0800

    create customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,29 @@
 from app import db
+import datetime
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String,nullable = False)
+    registered_at = db.Column(db.DateTime,default=(datetime.date.today()))
+    postal_code = db.Column(db.String)
+    phone = db.Column(db.String,nullable = False)
+
+
+
+    def to_dict(self):
+        customer_dict = {}
+        customer_dict["id"] = self.id
+        customer_dict["name"] = self.name
+        customer_dict["registered_at"] = self.registered_at
+        customer_dict["phone"] = self.phone
+        customer_dict["postal_code"] = self.postal_code
+        return customer_dict
+
+    @classmethod
+    def from_dict(cls, customer_data):
+        new_customer = Customer(name=customer_data["name"],
+                                phone=customer_data["phone"],
+                                postal_code = customer_data["postal_code"],
+                                registered_at = customer_data["registered_at"]
+                                )
+        return new_customer
