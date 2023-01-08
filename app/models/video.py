b976fbd19915abb9fc0fd5ebe56383696bfdddce
Jennifer Dai <jendai173@gmail.com>
2023-01-07 18:31:52 -0800

    add to_dict and from_dict back to customer and video models.

--- app/models/video.py
+++ app/models/video.py
@@ -7,6 +7,21 @@ class Video(db.Model):
     total_inventory = db.Column(db.Integer, default=0)
     customer = db.relationship("Customer", secondary="rental", backref="video")
 
+    def to_dict(self):
+        video_as_dict = {}
+        video_as_dict["id"] = self.id
+        video_as_dict["title"] = self.title
+        video_as_dict["release_date"] = self.release_date
+        video_as_dict["total_inventory"] = self.total_inventory     
+        return video_as_dict
+
+    @classmethod
+    def from_dict(cls, video_data):
+        new_video = Video(title = video_data["title"],
+                        release_date=video_data["release_date"],
+                        total_inventory = video_data['total_inventory'])
+        return new_video
+
     @classmethod
     def get_id(cls, id):
         return Video.query.get(id)
