fa2a6464d671321fc0bb4dd3337977b61cc20d97
kelly <kelly.tate.dev@gmail.com>
2023-01-08 07:12:16 -0800

    Refactors customer_routes. All tests passing

--- app/routes/customer_routes.py
+++ app/routes/customer_routes.py
@@ -9,15 +9,9 @@ customers_bp = Blueprint("customers_bp", __name__, url_prefix="/customers")
 
 @customers_bp.route("", methods=["POST"])
 def create_customer():
-    customer_data = request.get_json()
 
-    if "name" not in customer_data:
-        abort(make_response({"details": "Request body must include name."}, 400))
-    if "postal_code" not in customer_data:
-        abort(make_response({"details": "Request body must include postal_code."}, 400))
-    if "phone" not in customer_data:
-        abort(make_response({"details": "Request body must include phone."}, 400))
-
+    customer_data = validate_request_body(Customer, request.get_json())
+    
     new_customer = Customer(
         name=customer_data["name"],
         postal_code=customer_data["postal_code"],
@@ -28,12 +22,7 @@ def create_customer():
     db.session.add(new_customer)
     db.session.commit()
 
-    return make_response(jsonify({
-        "id": new_customer.id,
-        "name": new_customer.name,
-        "postal_code": new_customer.postal_code,
-        "phone": new_customer.phone,
-        "videos_checked_out_count": new_customer.videos_checked_out_count}), 201)
+    return make_response(jsonify(new_customer.to_dict()), 201)
 
 @customers_bp.route("", methods=["GET"])
 def get_customers():
@@ -75,14 +64,7 @@ def get_customers():
 @customers_bp.route("/<customer_id>", methods=["GET"])
 def get_one_customer(customer_id):
     customer = validate_model(Customer, customer_id)
-    return jsonify({
-        "id": customer.id,
-        "name": customer.name,
-        "registered_at": customer.registered_at,
-        "postal_code": customer.postal_code,
-        "phone": customer.phone,
-        "videos_checked_out_count": customer.videos_checked_out_count
-    })
+    return jsonify(customer.to_dict())
 
 @customers_bp.route("/<customer_id>",methods=["PUT"])
 def update_one_customer(customer_id):
@@ -99,12 +81,12 @@ def update_one_customer(customer_id):
 
 @customers_bp.route("/<customer_id>", methods=["DELETE"])
 def delete_one_customer(customer_id):
-    customer_info = validate_model(Customer, customer_id)
+    customer = validate_model(Customer, customer_id)
     
-    db.session.delete(customer_info)
+    db.session.delete(customer)
     db.session.commit()
     
-    return make_response(jsonify(customer_info.to_dict()), 200)
+    return make_response(jsonify(customer.to_dict()), 200)
 
 @customers_bp.route("/<customer_id>/rentals", methods=["GET"])
 def get_current_rentals(customer_id):
