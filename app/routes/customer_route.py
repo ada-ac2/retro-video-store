3d2bc1da1ec32a13227830c5b41e6f667ddb08cd
annadu <duyuan0921@gmail.com>
2023-01-08 12:25:45 -0800

    add create customer test pass

--- app/routes/customer_route.py
+++ app/routes/customer_route.py
@@ -1,6 +1,8 @@
 from app import db
 from app.models.customer import Customer
 from flask import Blueprint, jsonify, abort, make_response, request 
+from datetime import datetime
+
 
 customers_bp = Blueprint("customers_bp",__name__, url_prefix="/customers")
 
@@ -20,8 +22,22 @@ def validate_model(cls, model_id):
 #============================== planets_bp.route =============================
 #============================================================================
 #GET /customers
+@customers_bp.route("", methods=["POST"])
+def create_customer():
+    try:
+        request_body = request.get_json()
+        new_customer = Customer.from_dict(request_body)
+    except:
+        abort(make_response({"details":"Request body must include name.,Request body must include phone.,Request body must include postal_code."}, 400))
 
-GET /customers/<id>
-POST /customers
-PUT /customers/<id>
-DELETE /customers/<id>
+    db.session.add(new_customer)
+    db.session.commit()
+    
+    response_body = new_customer.to_dict()
+    return make_response(response_body, 201)
+# GET /customers/<id>
+# POST /customers
+
+
+# PUT /customers/<id>
+# DELETE /customers/<id>
