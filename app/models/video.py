970b865fcdd605d35ff651e95c5e95de5b3ac2f4
annadu <duyuan0921@gmail.com>
2023-01-08 09:30:19 -0800

    create video model

--- app/models/video.py
+++ app/models/video.py
@@ -1,4 +1,7 @@
 from app import db
 
 class Video(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    title = db.Column(db.String,nullable = False)
+    release_date = db.Column(db.String)
+    total_inventory = db.Column(db.Integer)
