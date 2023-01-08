92ba167f44c7da9bdbe6a9ab7ac796fb086d7a2e
annadu <duyuan0921@gmail.com>
2023-01-08 09:52:31 -0800

    create video from_dict

--- app/models/video.py
+++ app/models/video.py
@@ -14,3 +14,12 @@ class Video(db.Model):
         video_dict["total_inventory"] = self.total_inventory
         
         return video_dict
+
+    @classmethod
+    def from_dict(cls, video_data):
+        new_video = Video(title=video_data["title"],
+                                phone=video_data["phone"],
+                                total_inventory = video_data["total_inventory"]
+                                )
+        
+        return new_video
